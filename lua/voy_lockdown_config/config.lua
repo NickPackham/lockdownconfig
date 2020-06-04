040aedcf5ddcaa34f6d5b6e42bd78ae6a0c6ee78
Nick Packham <46549847+NickPackham@users.noreply.github.com>
2020-06-03 19:09:32 -0700

    Create config.lua

---
+++ lua/voy_lockdown_config/config.lua
@@ -0,0 +1,7 @@
+VOY_Lockdown_Console = {}
+
+VOY_Lockdown_Console_UnlockedDoors = { -- Doors which will be unable to be locked or unlocked by the Lockdown. Use 'mapcreationid' in console while looking at the door then enter the value in the table under the map name.
+	["rp_venator_extensive_v1_4"] = {
+		Doors = {4171, 1276},
+	},
+}
